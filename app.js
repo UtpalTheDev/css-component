7f2265031bb14a8d9417a2b6466400bbe00e0111
UtpalTheDev <utpalpati8@gmail.com>
2021-03-06 22:54:13 +0530

    refactor: icon button+ floating button

---
+++ app.js
@@ -0,0 +1,17 @@
+let float_btn=document.querySelector(".float-act-btn");
+let float_list=document.querySelector(".float-act-list");
+let float_btn_val=0;
+
+float_btn.addEventListener("click",floatClick);
+
+
+function floatClick(e){
+ if(float_btn_val===0)
+ {float_list.style.display=`block`;
+ float_btn.style.transform=`rotate(45deg)`;
+float_btn_val=1; }
+
+ else{float_list.style.display=`none`;
+ float_btn.style.transform=`rotate(90deg)`;
+ float_btn_val=0;}
+}
