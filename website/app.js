c3bf9379fb3f022dc547aec6cf9a5b60f557be55
UtpalTheDev <utpalpati8@gmail.com>
2021-03-23 20:05:58 +0530

    refactor: added button interaction for toasts

--- website/app.js
+++ website/app.js
@@ -1,14 +1,64 @@
 let float_btn=document.querySelector(".float-act-btn");
 let float_list=document.querySelector(".float-act-list");
 let float_btn_val=0;
+/*
+let toast_suc_elem=document.querySelector(".toast-success");
 
 
+let toastSuccess=document.querySelector(".toastSuccess");
+toastSuccess.addEventListener("click",()=>{
+    toast_suc_elem.classList.add("toastanim")
+    toastSuccess.style.cursor="not-allowed"
+setTimeout(()=>{toast_suc_elem.classList.remove("toastanim"); toastSuccess.style.cursor="pointer"
+
+},5000)});
+////////////////////////////////////////////////////////
+let toastError=document.querySelector(".toastError");
+
+let toast_err_elem=document.querySelector(".toast-error");
+
+toastError.addEventListener("click",()=>{
+    toast_err_elem.classList.add("toastanim")
+    toastError.style.cursor="not-allowed"
+setTimeout(()=>{toast_err_elem.classList.remove("toastanim"); toastError.style.cursor="pointer"
+
+},5000)});
+////////////////////////////////////////////////////
+
+let toastWarning=document.querySelector(".toastWarning");
+
+let toast_war_elem=document.querySelector(".toast-warning");
+
+toastWarning.addEventListener("click",()=>{
+    toast_war_elem.classList.add("toastanim")
+    toastWarning.style.cursor="not-allowed"
+setTimeout(()=>{toast_war_elem.classList.remove("toastanim"); toastWarning.style.cursor="pointer"
+
+},5000)});
+
+//////////////////////////////////////////
+
+let toastInfo=document.querySelector(".toastInfo");
+let toast_info_elem=document.querySelector(".toast-info");
+
+toastInfo.addEventListener("click",()=>{
+    toast_info_elem.classList.add("toastanim")
+    toastInfo.style.cursor="not-allowed"
+setTimeout(()=>{toast_info_elem.classList.remove("toastanim"); toastInfo.style.cursor="pointer"
+
+},5000)});
+
+*/
 let ham_btn=document.querySelector(".ham");
+
 let sidenav=document.querySelector(".sidenav");
+
 let sidenav_close=document.querySelector(".sidenav-closebtn");
 
 float_btn.addEventListener("click",floatClick);
+
 ham_btn.addEventListener("click",sidenavopen);
+
 sidenav_close.addEventListener("click",sidenavclose);
 
 function floatClick(e){
